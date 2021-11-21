0b4914f1cbb2285463994792a9d796e73a02ade8
noelopa <89892964+noelopa@users.noreply.github.com>
2021-11-20 22:53:28 -0600

    marcas y backend de modulo productos Noe

--- AppWeb/persianas/applications/productos/models.py
+++ AppWeb/persianas/applications/productos/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import models, migrations
+from django.contrib.postgres.fields import HStoreField
+from django.contrib.postgres.operations import HStoreExtension, UnaccentExtension
+from django.contrib.auth.models import User
 
+class Migration(migrations.Migration):
+	operations = [
+		HStoreExtension(),
+		UnaccentExtension(),
+	]
+    
 # Create your models here.
+class Marca(models.Model):
+	name = models.CharField(max_length=20)
