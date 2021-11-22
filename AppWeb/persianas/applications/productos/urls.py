957ab9ad2ee5a8bdd05615ba826dacd5a377a5a7
Ordaz Romero Sergio <orcheko@gmail.com>
2021-11-22 19:34:36 +0000

    Model Products, products js

--- AppWeb/persianas/applications/productos/urls.py
+++ AppWeb/persianas/applications/productos/urls.py
@@ -5,4 +5,5 @@ from . import views as vw
 
 urlpatterns = [
 	path('productos/', vw.productos),
+	path('productos/info', vw.curdproductos.as_view()),
 ]
--- AppWeb/persianas/static/js/productos/productos.js
+++ AppWeb/persianas/static/js/productos/productos.js
@@ -1,56 +1,46 @@
-const peticiones = apiRequests('peticion/admins')
-
-window.onload = () => {
-	
-}
-
-function initializeDataTable()
-{
-	// Setup - add a text input to each footer cell
-	$('#example-1 tfoot th').each( function () {
-		var title = $(this).text();
-		$(this).html( '<input class="form-control" type="text" placeholder="Buscar '+title+'">' );
+$(document).ready(function(){
+		initializeDataTable();
 	});
 	
-	// DataTable
-	var table = $('#example-1').DataTable({
-		'ajax': {
-		   url: "/productos/curd",
-		   type: "get",
-		   data: function (data) {
-			   // let min = parseInt($('#minFilter').val().trim())
-			   // let max = parseInt($('#maxFilter').val().trim())
-			   // data = $('#filter').attr('data')
-			   // data = {data, min, max}
-			   // data = {data: JSON.stringify(data)}
-			   // return data
-		   },
-		   "dataSrc": function (json) {
-			   $(json.data).each((position, value) => {
-				   // let actions = value[6][0] + value[7] + value[6][1] + value[7] + value[6][2] + value[8] + value[6][3] + value[6][4] + value[7] + value[6][5]
-				   // value[6] = actions
-			   })
-			   return json.data;
-		   },
-		},
-		'scrollX': true,
-		"drawCallback": function( settings ) {
-			let rows
-			rows = $('.waring_info[data="1"]').parent().parent()
-			rows.css('background-color', 'rgba(255, 50, 0, 0.5)')
-		}
-	});
-	
-	// Apply the search
-	table.columns().every( function () {
-		var that = this;
-		
-		$( 'input', this.footer() ).on( 'keyup change', function () {
-			 if ( that.search() !== this.value ) {
-			   that.search( this.value ).draw();
-			 }
+	function initializeDataTable()
+	{
+		// Setup - add a text input to each footer cell
+		$('#example-1 tfoot th').each( function () {
+			var title = $(this).text();
+			$(this).html( '<input class="form-control" type="text" placeholder="Buscar '+title+'">' );
 		});
-	});
-	
-	$('[placeholder="Buscar Acciones"]').prop( "disabled", true );
-}
+		
+		// DataTable
+		var table = $('#example-1').DataTable({
+			'ajax': {
+			   url: "/cangeables/info",
+			   type: "get",
+			   "dataSrc": function (json) {
+				   $(json.data).each((position, value) => {
+					   let actions = value[6][0] + value[7] + value[6][1]
+					   value[6] = actions
+				   })
+				   return json.data;
+			   },
+			},
+			'scrollX': true,
+			"drawCallback": function( settings ) {
+				let rows
+				rows = $('.waring_info[data="1"]').parent().parent()
+				rows.css('background-color', 'rgba(255, 50, 0, 0.5)')
+			}
+		});
+		
+		// Apply the search
+		table.columns().every( function () {
+			var that = this;
+			
+			$( 'input', this.footer() ).on( 'keyup change', function () {
+				 if ( that.search() !== this.value ) {
+				   that.search( this.value ).draw();
+				 }
+			});
+		});
+		
+		$('[placeholder="Buscar Acciones"]').prop( "disabled", true );
+	}
